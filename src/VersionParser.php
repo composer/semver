9b7a27a5173fe8e40a6c8c6098ad25b2690ec671
Rob Bast <rob.bast@gmail.com>
2015-08-03 14:27:38 +0200

    removed `VersionParser::parseNameVersionPairs`
    only used by create-project and init, not integral for semver lib

--- src/VersionParser.php
+++ src/VersionParser.php
@@ -500,34 +500,4 @@ class VersionParser
                 return $stability;
         }
     }
-
-    /**
-     * Parses a name/version pairs and returns an array of pairs.
-     *
-     * @param array $pairs a set of package/version pairs separated by ":", "=" or " "
-     *
-     * @return array[] array of arrays containing a name and (if provided) a version
-     */
-    public function parseNameVersionPairs(array $pairs)
-    {
-        $pairs = array_values($pairs);
-        $result = array();
-
-        for ($i = 0, $count = count($pairs); $i < $count; ++$i) {
-            $pair = preg_replace('{^([^=: ]+)[=: ](.*)$}', '$1 $2', trim($pairs[$i]));
-            if (false === strpos($pair, ' ') && isset($pairs[$i + 1]) && false === strpos($pairs[$i + 1], '/')) {
-                $pair .= ' ' . $pairs[$i + 1];
-                ++$i;
-            }
-
-            if (strpos($pair, ' ')) {
-                list($name, $version) = explode(' ', $pair, 2);
-                $result[] = array('name' => $name, 'version' => $version);
-            } else {
-                $result[] = array('name' => $pair);
-            }
-        }
-
-        return $result;
-    }
 }
