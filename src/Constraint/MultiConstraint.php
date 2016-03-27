c7f6745cbb24e32d540df42293baf125e84a318a
Andrii Vasyliev <sol@hiqdev.com>
2016-03-27 16:35:12 +0000

    Add getters for constraint and conjunctive in MultiConstraint

--- src/Constraint/MultiConstraint.php
+++ src/Constraint/MultiConstraint.php
@@ -36,6 +36,30 @@ class MultiConstraint implements ConstraintInterface
     }
 
     /**
+     * @return ConstraintInterface[]
+     */
+    public function getConstraints()
+    {
+        return $this->constraints;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isConjunctive()
+    {
+        return $this->conjunctive;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDisjunctive()
+    {
+        return !$this->conjunctive;
+    }
+
+    /**
      * @param ConstraintInterface $provider
      *
      * @return bool
