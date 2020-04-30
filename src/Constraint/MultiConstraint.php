fa8fd4df1cb8a9d382288be1cbc287ec364a5aac
Jérémy Derussé <jeremy@derusse.com>
2020-04-30 15:49:47 +0200

    Invert comparison

--- src/Constraint/MultiConstraint.php
+++ src/Constraint/MultiConstraint.php
@@ -111,7 +111,7 @@ class MultiConstraint implements CompilableConstraintInterface
     {
         if (false === $this->conjunctive) {
             foreach ($this->constraints as $constraint) {
-                if ($constraint->matches($provider)) {
+                if ($provider->matches($constraint)) {
                     return true;
                 }
             }
@@ -120,7 +120,7 @@ class MultiConstraint implements CompilableConstraintInterface
         }
 
         foreach ($this->constraints as $constraint) {
-            if (!$constraint->matches($provider)) {
+            if (!$provider->matches($constraint)) {
                 return false;
             }
         }
